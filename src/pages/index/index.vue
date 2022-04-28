6a656c49514fd31f412a5c083206c7a500a1e5db
Stephen <zzyxiaohui@163.com>
2022-04-28 12:11:30 +0800

    引入feeds组件报错

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,6 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
+		<feeds />
 	</view>
 </template>
 
@@ -22,28 +19,3 @@
 		}
 	}
 </script>
-
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
-</style>
